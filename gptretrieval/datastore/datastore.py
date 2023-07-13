be129e9846c190ba4d582f7267f354ea864b1311
Donald Tolley <tolleybot@gmail.com>
2023-07-13 14:19:54 -0400

    adding get_embddings to datastore to allow override

--- gptretrieval/datastore/datastore.py
+++ gptretrieval/datastore/datastore.py
@@ -8,17 +8,20 @@ from ..models.models import (
     QueryWithEmbedding,
 )
 
-from ..services.openai import get_embeddings
+from ..services import openai
 
 
 class DataStore(ABC):
+    def get_embeddings(self, texts: List[str]) -> List[List[float]]:
+        return openai.get_embeddings(texts)
+
     async def query(self, queries: List[Query], top_k=10) -> List[QueryResult]:
         """
         Takes in a list of queries and filters and returns a list of query results with matching document chunks and scores.
         """
         # get a list of of just the queries from the Query list
         query_texts = [query.query for query in queries]
-        query_embeddings = get_embeddings(query_texts)
+        query_embeddings = self.get_embeddings(query_texts)
         # hydrate the queries with embeddings
         queries_with_embeddings = [
             QueryWithEmbedding(**query.dict(), embedding=embedding)
